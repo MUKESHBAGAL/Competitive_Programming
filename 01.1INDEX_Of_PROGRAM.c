b02d5f4da63f69c8ea5d942c00f4e26e8367d293
MUKESHBAGAL <2022bit039@sggs.ac.in>
2024-06-08 12:38:48 +0530

    Reading Statement input using scanset

---
+++ 01.1INDEX_Of_PROGRAM.c
@@ -0,0 +1,27 @@
+PROBLEMS SOLVED TILL NOW (IN SHORT)
+
+1) Reading Statement input using scanset
+2) Printing statement Each word line by line
+3) Printing Each Word in Step manner
+4) Printing by reversing Every 2 Consentive char
+5) Perfect or not
+6) Deficient or not
+7) Strong or not
+8) Armstrong or not
+9) Print Palindromes up to given N
+10) Print Fibonacci Series
+11) Print Reverse of given number
+12) Binary to Gray Code
+13) Gray Code to Binary
+14) Decimal To Binary
+15) Binary to Decimal
+16) Binary to octal
+17) Binary to Hexa  Decimal
+18) Hexa Decimal to octal
+19) Print Number Series 0,1,3,6,10,15---- N
+20) Sum of Series 1, 1/2, 1/3, 1/4---- 1/7
+21) N,K given, Print Series by subtracting k from N upto 5=0 Then Reverse
+22) Octal to Decimal
+23) Decimal to octal
+24) Decimal to Hexa  Decimal
+25) Find the given Number is Fibonacci or Not
