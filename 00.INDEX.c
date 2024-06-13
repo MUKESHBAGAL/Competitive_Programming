b07b1eb8a9366807d58c075a8c84c17bc2a5e1bc
MUKESHBAGAL <2022bit039@sggs.ac.in>
2024-06-13 17:01:02 +0530

    INDEX OF PROGRAM

---
+++ 00.INDEX.c
@@ -0,0 +1,46 @@
+                                                                                   *****INDEX OF COMMPETITIVE PROGRAMMING*****
+                                                                                   
+                                                                                   
+		###BASIC PROGRAM### (01 To 27)                                                                           
+01.Write 'C' Code that accepts a string stras input and print each word of the string in new line.
+02.Write 'C' Code that accepts a string str as input and print each word of the string except the last character                                                                             
+03.Write 'C' Code that accepts a string str as input and print each word of the string in Step Format.
+04.Write 'C' Code that Calculates Sum of 3 given numbers with input entered in one line separated by comma.
+05.Write 'C' Code that prints the given number is EVEN or ODD without using Conditional Statements and % operator
+06.Write 'C' Code that prints the given number, N is Triangular Number or Not.
+07.Write 'C' Code that prints the given number, N is Perfect or Not
+08.Write 'C' Code that prints the All Mersenne prime numbers up to given N Value.
+09.Write 'C' Code that prints the All Palindromes up to given Limit, limit.
+10.Write 'C' Code that find out the given number is Fibonacci Number or Not.
+11.Write 'C' Code that accepts a string str as input and print each word of the string in Reverse.
+12.Write 'C' Code that accepts a Number N as input and find whether it is Deficient or Not.
+13.Write 'C' Code that accepts a string str as input and print the string by swapping each pair of characters.
+14.Write 'C' Code that accepts a Number N as input and find whether it is Strong or Not.
+15.Write 'C' Code to reverse the digits of a number using recursion
+16.Write 'C' Code that Converts the given Decimal number to binary and print the same in 16-bit form.
+17.Write 'C' Code that Converts the given Decimal number to Octal and hexa decimal and print the same.
+18.Write 'C' Code that Converts the given Octal Integer to Decimal Integer without using format specifier and pow() function
+19.Write 'C' Code that Converts the given Binary value to Octal Integer.
+20.Write 'C' Code that Converts the given Binary Code to Gray Code.
+21.Write 'C' Code that calculate the sum of the series 1, 1/2, 1/3, 1/4, 1/5, 1/6, ......, 1/n.
+22.Write 'C' program For given two numbers N and K, our task is to subtract a number K from N until number(N) is greater than zero, once the N becomes negative or zero then we start adding K until that number become the original number(N).
+23.Write 'C' Code that Converts the given Hexa Decimal Integer to Octal Integer.
+24.Write 'C' Code that find the given number is armstrong number or not.
+25.Write 'C' Code that Converts the given Gray Code to Binary Number.
+26.Write 'C' Code to print fibonacci series upto given N value.
+27.Write 'C' Code to print Number Series 0,1,3,6,10,15,... Ν.
+
+		###PATTERN PRINTING ###(28 To )
+28.Write 'C' Code to print Square Pattern with given character.
+29.Write 'C' Code to print half pyramid with given character as given below.
+30.Write 'C' Code to print Square Pattern as given below.
+31.Write 'C' Code to print Full Pyramid Pattern as shown below.
+32.Write 'C' Code to print Inverted half pyramid pattern as given below.
+33.Write 'C' Code to print Mirrored Right angle Triangle pattern as given below.
+34.Write 'C' Code to print Number Pyramid pattern as given below
+35.Write 'C' Code to print Palindrome Number Pyramid pattern as given below.
+36.Write 'C' Code to print Mirrored Arrow Shaped pattern as given below.
+37.Write 'C' Code to print Hollow square star pattern with Diagonal
+38.Write 'C' Code to print butterfly pattern as given below.
+39.Write 'C' Code to print Right angle Triangle pattern as given below.
+40.Write 'C' Code to print Hallow Full Pyramid pattern as given below.
